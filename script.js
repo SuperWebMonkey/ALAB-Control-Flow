1de0b69cff99208385dfa34fc17c4d7b1c856d79
SuperWebMonkey <vikingsattack@outlook.com>
2024-09-12 10:12:32 -0700

    adding base variables

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ *
+ *
+ *
+ */
+
+// variables
+const PI = 3.1415;
+let area = PI * radius * radius;
+let radius = 5;
+let plantSpace = 0.8;
+let areaStar = 20;
+
+// Plant Growth Control Flow
+let weeks = 0;
+let percentage = Math.pow(2, weeks);
